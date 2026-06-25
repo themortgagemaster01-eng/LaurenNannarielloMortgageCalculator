e012d758bda5e702527ca8d2fd2877b1495fc715
themortgagemaster01-eng <themortgagemaster01@gmail.com>
2026-06-25 13:20:36 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ln-mortgage-v1';
+const CACHE='ln-mortgage-v9';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
